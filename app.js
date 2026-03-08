fabfe44710755cdae237460778bd61f57b68b612
caleb <calebtelfe@gmail.com>
2026-03-08 10:58:55 -0400

    file structure setup + app.js basic config

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//ejs config
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+
+//start server
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log(`Express app listening on port ${PORT}`);
+})
